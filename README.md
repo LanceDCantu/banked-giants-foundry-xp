1385459289d6de8a5508f7d79b3fa4677cef7e4a
unknown <lance@nevada.unr.edu>
2023-09-25 23:00:10 -0400

    Updating code from banked-experience plugin

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Example
 An example greeter plugin
+
+https://github.com/runelite/plugin-hub#creating-new-plugins
--- src/test/java/com/giantsfoundrybankedexperience/GiantsFoundryBankedExperiencePluginTest.java
+++ src/test/java/com/giantsfoundrybankedexperience/GiantsFoundryBankedExperiencePluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.giantsfoundrybankedexperience;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class GiantsFoundryBankedExperiencePluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(GiantsFoundryBankedExperiencePlugin.class);
 		RuneLite.main(args);
 	}
 }
